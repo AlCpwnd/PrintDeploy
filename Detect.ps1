647079f548fdf5457390b508a91ab9977cd2b345
AlCpwnd <a.dejaeger@hotmail.com>
2025-07-11 16:46:07 +0200

    Cleaned up script and added documentation

--- Detect.ps1
+++ Detect.ps1
@@ -1,8 +1,24 @@
+$printerName = '<Printer Name>'
+
 try{
-    Get-Printer -Name '<PrinterName>' -ErrorAction Stop
+    Get-Printer -Name $printerName -ErrorAction Stop
     $ExitCode = 0
 }catch{
     $ExitCode = 1
 }
 
 exit $ExitCode
+
+
+<#
+    .SYNOPSIS
+    Script meant for confirming printer installation.
+
+    .DESCRIPTION
+    Modify the "$printerName" variable to correspond to
+    the printer you want to confirm the installation of,
+    and upload it to the detection rules of your app.
+
+    .LINK
+    Get-Printer
+#>
